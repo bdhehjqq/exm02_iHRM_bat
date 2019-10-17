851d5e30951039461c633d06c18d7e5f1ba1c1e7
YourName <email@example.com>
2019-10-17 15:32:02 +0800

    版本v1.0

---
+++ run_suite.py
@@ -0,0 +1,30 @@
+"""
+    组织测试套件实现 登录 与员工增删改查 接口的串联
+"""
+# 导包
+import unittest
+
+from case.TestIHRMEmploye import TestEmployee
+from case.TestIHRMUser import TestUser
+from tools.HTMLTestRunner import HTMLTestRunner
+
+# 组织测试套件对象
+
+suite = unittest.TestSuite()
+suite.addTest(TestUser("test_login_success"))  # 登录成功
+suite.addTest(TestEmployee("test_emp_add"))
+suite.addTest(TestEmployee("test_emp_update"))
+suite.addTest(TestEmployee("test_emp_get"))
+suite.addTest(TestEmployee("test_emp_delete"))
+
+# 执行套件对象
+runner = unittest.TextTestRunner()
+runner.run(suite)
+
+# 打开文件流
+with open("./report/" + "report.html", "wb") as f:
+    # 使用 HTMLTestRunner 要运行测试套件，将结果写入文件流
+    runner = HTMLTestRunner(f, title="我的测试报告", description="测试TPShop登录接口")
+    runner.run(suite)
+
+
